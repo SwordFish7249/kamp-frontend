ccfb76795137902061705dab551e11d6c95c8fa2
CEM G <g.raing8@hotmail.com>
2021-03-13 14:46:15 +0200

    kamp-frontend cardata

---
+++ src/app/models/car.ts
@@ -0,0 +1,12 @@
+export interface Car{
+    id:number;
+    brandName:string;
+    modelYear:string;
+    colorName:string;
+    carName:string;
+    carModel:string;
+    carEngine:string;
+    carType:string;
+    unitsInStock:number;
+    unitPrice:number;
+}
